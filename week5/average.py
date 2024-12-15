927019cb9c58a40c2e5aaa5fe3b46aa3334aea05
nischal magar <manischal23@tbc.edu.np>
2024-12-15 21:15:52 +0545

    week5

---
+++ week5/average.py
@@ -0,0 +1,7 @@
+import sys
+
+values = sys.argv[1:]
+print (values)
+def hello():
+    inp=input("enter the name")
+    print(f"your name is {inp}")
---
+++ week5/math.py
@@ -0,0 +1,11 @@
+# import  math
+# import sys
+# import average
+# print(sys.path)
+# print(dir(math))
+# average.hello()
+# argvar=sys.argv[1:]
+# print(argvar)
+# print(sorted(argvar))
+# sortvar=sorted(argvar,key=len)
+# print(sortvar)
