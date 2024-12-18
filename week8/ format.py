cf3a29ff18e2b1b7c6a1d70a579f1551e11a85e8
nischal magar <manischal23@tbc.edu.np>
2024-12-18 21:34:13 +0545

    week8

---
+++ week8/ format.py
@@ -0,0 +1,29 @@
+# name="ram"
+# age=12
+# print(f"mu name is {name} age is {age}")
+# print(f"{'python':*^10}")
+# print(f"{42:+}")
+# print(f"{42:}")
+# print(f"{42:-}")
+# print(f"{1:#b}")
+# print(f"{10:#o}")
+# print(f"{12:010}")
+# print(f"{12345674534:,}")
+# print(f"{12345674534:_}")
+# print(f"{12345674534:}")
+# nam=[("ram",12),("shyam",14)]
+# for name,age in nam:
+#     print(f"{name:8}------>{age: 6}")
+# k=f"{age:019}"
+# print(k)
+# print(f"{-122:*=10}")
+# print(f"{-122:*^10}")
+# print(f"{-122:*<10}")
+# print(f"{12:0>8x}")
+# item="bread"
+# cost=12.1
+# print(f"{item:^11} {cost :08.2f}")
+# print("value{:3}".format(age))
+# print("name".center(10))
+
+
---
+++ week8/file.py
@@ -0,0 +1,21 @@
+# f =open("text.txt","r+")
+# h=f.append("my name is hello")
+# k=f.append("hello")
+
+
+# first=f.readline()
+# second=f.readline()
+# print(first.strip())
+# print(second.strip())
+# lines=f.readlines()
+
+# #only can write in string
+# for line in lines:
+#     print(line.strip())
+# print(type(f))
+# f.tell()
+# f.seek(0,0)
+# f.write("a")
+# f.close()
+with open("tell.txt") as f:
+    
